f97328a603c0f912513a9211f0e1879e3b9a15b1
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-18 18:59:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,8 +74,11 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   
   holiday_hash.each do |season, holidays|
-  holidays.each do |
+    holidays.each do |holiday, supplies|
+      if supplies == "BBQ"
+      end
     binding.pry
+    end
   end
 end
 
